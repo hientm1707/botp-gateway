2ffdd527741b4762db97ab1083dd84fc25a6edc3
HienTran <minhhien1772000@gmail.com>
2022-03-13 23:48:29 +0700

    init project

---
+++ src/main/java/vn/edu/hcmut/BotpGatewayApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotpGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/hcmut/BotpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotpGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
